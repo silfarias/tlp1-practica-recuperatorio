a886819cbf057c5fb55d8fa6b8310786574681d9
silfarias17 <silvifarias17@gmail.com>
2023-07-03 09:06:41 -0300

    parcial terminado

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors());
-//app.use(helmet());
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
